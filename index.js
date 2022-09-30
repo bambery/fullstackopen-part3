dffac56367a2c424710b568989c55c5b0bee0770
bambery <lwszolek@gmail.com>
2022-09-30 16:43:46 -0500

    Completed exercise 3.7 - Phonebook API: add package morgan for logs

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const crypto = require('crypto')
+const morgan = require('morgan')
 
 const app = express()
 app.use(express.json())
+
+app.use(morgan('tiny'))
 
 let persons = [
     {
