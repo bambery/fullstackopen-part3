e1b196977b35ba345612e72cea36d93b215cb881
bambery <lwszolek@gmail.com>
2022-09-30 12:38:17 -0500

    Completed exercise 3.5 - Phonebook API: POST /api/persons endpoint

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
 const express = require('express')
-const app = express()
+const crypto = require('crypto')
 
+const app = express()
 app.use(express.json())
 
 let persons = [
-    { 
+    {
       "id": 1,
-      "name": "Arto Hellas", 
+      "name": "Arto Hellas",
       "number": "040-123456"
     },
-    { 
+    {
       "id": 2,
-      "name": "Ada Lovelace", 
+      "name": "Ada Lovelace",
       "number": "39-44-5323523"
     },
-    { 
+    {
       "id": 3,
-      "name": "Dan Abramov", 
+      "name": "Dan Abramov",
       "number": "12-43-234345"
     },
-    { 
+    {
       "id": 4,
-      "name": "Mary Poppendieck", 
+      "name": "Mary Poppendieck",
       "number": "39-23-6423122"
     }
 ]
+
+const generateId = () => {
+    // the exercise says to use Math.random(), but es6 offers the crypto api which is better designed for this situation:
+    const array = new Uint32Array(1)
+    return crypto.getRandomValues(array)[0]
+}
 
 app.get('/info', (request, response) => {
     const timestamp = new Date
 //    response.send(`<h1>Hello World! ${Date.now()}</h1>`)
     response.send(`<div>Phonebook has info for ${persons.length} people.</div><div>${timestamp.toString()}</div>`)
 })
-    
+
 app.get('/api/persons/', (request, response) => {
     response.json(persons)
 })
@@ -39,7 +46,7 @@ app.get('/api/persons/', (request, response) => {
 app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     const person = persons.filter(person => person.id === id)
-   
+
     if(person) {
         response.json(person)
     } else {
@@ -50,8 +57,29 @@ app.get('/api/persons/:id', (request, response) => {
 app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     persons = persons.filter(person => person.id !== id)
-    
+
     response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    console.log(`*** body: ${body.name}`)
+
+    if (!(body.name && body.number)) {
+        return response.status(400).json({
+            error: 'missing name or number'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
 })
 
 const PORT = 3001
